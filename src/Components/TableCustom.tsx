9ed4f0d107e40d3e5da344217372a3af02520d6b
Yin Chantha <YinChantha>
2023-01-18 22:43:12 +0700

    update main

--- src/Components/TableCustom.tsx
+++ src/Components/TableCustom.tsx
@@ -1,6 +1,8 @@
 import React, { HTMLAttributes, TdHTMLAttributes } from "react";
-import { DefaultRecordType, GetRowKey } from "rc-table/lib/interface";
+import { DefaultRecordType } from "rc-table/lib/interface";
 import Table from "rc-table";
+import "../Components/tablestyle.css"
+
 import {
   ColumnsType,
   ColumnGroupType,
@@ -8,30 +10,29 @@ import {
 } from "rc-table/lib/interface";
 const bgRowColor = (record: any, index: number, indent: number): string => {
   if (index % 2 === 0) {
-    return "bg-gray-200";
+    return "bg-gray-100";
   }
-  return "bg-gray-100";
+  return "bg-gray-0"
 };
 
 const onHeaderRow = (record: any, index: number | undefined) => {
   const props: HTMLAttributes<any> | TdHTMLAttributes<any> = {
-    className: "bg-sky-300",
+    // className: "bg-sky-300",
+    className: "bg-blue-300"
   };
   return props;
 };
 
+
 const TableCustom = ({
   data,
   columns,
-  rowKey,
 }: {
   data: readonly DefaultRecordType[] | undefined;
   columns: ColumnsType<ColumnGroupType<any> | ColumnType<any>> | undefined;
-  rowKey?: string | GetRowKey<DefaultRecordType> | undefined;
 }) => {
   return (
     <Table
-      rowKey={rowKey}
       onHeaderRow={onHeaderRow}
       columns={columns}
       data={data}
