43d8a21f23b15cbe7b20fd49a918785f2593814c
NeavKimpor <kimporneav27@gmail.com>
2023-01-19 17:48:44 +0700

    solve conflict

--- src/Pages/Admin/Facultysalary.tsx
+++ src/Pages/Admin/Facultysalary.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Facultysalary = () => {
   return (
-    <div>
-      Faculty Salray!!!
+    <div className='p-10'>
+      <div className="text-3xl text-center font-bold">Faculty Salary</div>
     </div>
   )
 }
